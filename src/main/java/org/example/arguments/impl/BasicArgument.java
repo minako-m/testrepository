7cabbfb7208f80e21942066f77ed0a8d1533ddc7
tkachenko_26607 <artur.tkachenko@kaspi.kz>
2022-09-24 20:39:40 +0600

    Renamed simple argument

--- src/main/java/org/example/arguments/impl/BasicArgument.java
+++ src/main/java/org/example/arguments/impl/BasicArgument.java
@@ -2,10 +2,10 @@ package org.example.arguments.impl;
 
 import org.example.arguments.Argument;
 
-public class SimpleArgument implements Argument {
+public class BasicArgument implements Argument {
     private final String value;
 
-    public SimpleArgument(String value) {
+    public BasicArgument(String value) {
         this.value = value;
     }
 
